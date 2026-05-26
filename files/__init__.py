f6515b547d37c89cde0f2c90731dbe49178440c4
Benajamin Locher <blocher@mlaglobal.com>
2026-05-26 17:01:48 -0400

    removing duplicate code

--- files/__init__.py
+++
@@ -1,9 +0,0 @@
-"""File helpers for SimpleAI."""
-
-from .extractor import collect_file_paths, extract_text_from_file, extract_text_from_files
-
-__all__ = [
-    "collect_file_paths",
-    "extract_text_from_file",
-    "extract_text_from_files",
-]
--- management/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Django management integration for simpleai."""
